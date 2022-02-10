f0c5c2e18758b2240b7e8adf9baf14ae114cc895
Takeru <itstudentkz2000@gmail.com>
2022-02-10 19:46:21 +0600

    fourth commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,24 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
 
-let chosenMaxLife = 100;
+let chosenMaxLife = 5;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
 function onAttack() {
-    const damage = dealMonsterDamage(ATTACK_VALUE);
+    const damage = dealMonsterDamage(ATTACK_VALUE); 
     currentMonsterHealth -= damage;
+    const monsterDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= monsterDamage;
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0){
+        alert('You won!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You lost!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0 ) {
+        alert('DRAW');
+    }
 }
 
 attackBtn.addEventListener('click', onAttack);
